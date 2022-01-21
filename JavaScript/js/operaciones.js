fea4a40a191846d3e97b5536429d76708297bdb7
topgirl2410 <maria.topgirl82@gmail.com>
2022-01-21 22:25:06 +0100

    operaciones js

---
+++ JavaScript/js/operaciones.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Orden de las operaciones;
+
+let resultado;
+
+resultado = (20 + 30) * 2;
+
+resultado = (100 + 200 + 300) * .2; // Asi podemos obtener el tanto por ciento de la supuesta compra.
+
+
+console.log(resultado);
+
+
+// Incrementos 
+
+let like = 10;
+like++;
+
+
+console.log(like)
