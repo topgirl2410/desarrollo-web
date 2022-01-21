8b104e998891c0133be65d12752963c29cf0b745
topgirl2410 <maria.topgirl82@gmail.com>
2022-01-21 22:43:26 +0100

    template y boolean

---
+++ JavaScript/js/booleans.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Boolean (true o false)
+
+const boolean1 = true;
+const boolean2 = false;
+
+
+console.log(typeof boolean1); // Tipo de valor que contiene;
+console.log(boolean2);
---
+++ JavaScript/js/template-strings.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const nombre = 'María';
+const email = "correo@correo.com";
+
+/**
+ * Concatenación;
+ */
+
+console.log("Nombre Cliente: " + nombre + " Email: " + email) // Concatenación;
+
+
+/**
+ * Template Strings - Strings Literals;
+ */
+
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`)  // Manera correcta de hacerlo;
+
