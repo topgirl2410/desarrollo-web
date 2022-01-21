92acd0779dbb92669f5e9699ef96b1b3c81574ee
topgirl2410 <maria.topgirl82@gmail.com>
2022-01-21 21:54:31 +0100

    acabado numeros en js

--- JavaScript/js/numeros.js
+++ JavaScript/js/numeros.js
@@ -5,5 +5,12 @@
 const numero1 = 100;
 const numero2 = 200;
 
-console.log(numero1);
-console.log(numero2);
+
+
+
+
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero2 * numero1);
+console.log(numero2 / numero1);
+console.log(numero2 % numero1);
