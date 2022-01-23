77e702a8777fd2d32ab2da1521ba92657f797c5c
topgirl2410 <maria.topgirl82@gmail.com>
2022-01-23 21:59:15 +0100

    forEach, includes y some

--- JavaScript/js/metodosArrays2.js
+++ JavaScript/js/metodosArrays2.js
@@ -1,3 +1,35 @@
 'use strict';
 
-const meses = ['Enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio'];
+const meses = ['Enero', 'febrero', 'marzo', 'abril', 'mayo'];
+
+const carrito = [
+    { nombre: 'Monitor 20 pulgadas', precio: 300 },
+    { nombre: 'Televisión 50 pulgadas', precio: 600 },
+    { nombre: 'Tablet', precio: 300 },
+    { nombre: 'Audifonos', precio: 200 },
+    { nombre: 'Teclado', precio: 50 },
+    { nombre: 'Telefono movil', precio: 500 },
+    { nombre: 'Laptop', precio: 800 },
+    { nombre: 'Altavoces', precio: 300 },
+]
+
+// forEach 
+
+meses.forEach(function (mes) {
+    if (mes == 'marzo') {
+        console.log('marzo si existe');
+    }
+});
+
+// Includes (arrays planos como el de meses)verificar si existe o no
+
+let resultado = meses.includes('marzo');
+
+// Some ideal para arrays de objetos 
+
+resultado = carrito.some(function (producto) {
+    return producto.nombre === 'Telefono movil' // acceder a cualquier propiedad en este caso nombre
+});
+
+
+console.log(resultado);
