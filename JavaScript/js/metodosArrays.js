275c0d4fe468fecebf5a1c0a28326030e251f8ac
topgirl2410 <maria.topgirl82@gmail.com>
2022-01-23 12:11:08 +0100

    splice, eliminar elementos

--- JavaScript/js/metodosArrays.js
+++ JavaScript/js/metodosArrays.js
@@ -5,10 +5,6 @@
 const numeros = [10, 20, 30, 40, 50];
 
 
-const meses = ['Enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio'];
-
-console.table(meses);
-
 // Metodos para Arrays;
 
 numeros.push(60, 70, 80, 90); // agrega los elementos al final de array;
@@ -16,3 +12,15 @@ numeros.push(60, 70, 80, 90); // agrega los elementos al final de array;
 numeros.unshift(-10, -20, -30); // agrega los elementos al principio del array;
 
 console.table(numeros) // Muestra datos tabulares como una tabla.
+
+
+
+const meses = ['Enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio'];
+
+//meses.pop(); // elimina el ultimo elemento del array;
+
+//meses.shift(); // elimina el primer elemento del array;
+
+meses.splice(2, 1); // elimina el elemento que tu le indiques, primero es la posicion del elemento, segundo cuantos quieres borrar desde alli;
+
+console.table(meses);
