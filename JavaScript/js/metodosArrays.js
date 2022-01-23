32caf9011a9e9551583868d3e4cc8716491321d4
topgirl2410 <maria.topgirl82@gmail.com>
2022-01-23 12:02:20 +0100

    metodos con arrays

---
+++ JavaScript/js/metodosArrays.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Arrays;
+
+const numeros = [10, 20, 30, 40, 50];
+
+
+const meses = ['Enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio'];
+
+console.table(meses);
+
+// Metodos para Arrays;
+
+numeros.push(60, 70, 80, 90); // agrega un elemento al array;
+
+
+console.table(numeros) // Muestra datos tabulares como una tabla.
