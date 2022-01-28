2fd27fd1415c65842d6c57a86f88d1bcc16a5817
topgirl2410 <maria.topgirl82@gmail.com>
2022-01-28 12:49:05 +0100

    arrow function

---
+++ JavaScript/js/arrowFunction.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Arrow Functions
+
+const sumar2 = (n1, n2) => console.log(n1 + n2);
+
+
+sumar2(5, 10);
+
+const aprendiendo = (tecnologia) => {
+    console.log(`Aprendiendo ${tecnologia}`)
+}
+
+aprendiendo('JavaScript')
