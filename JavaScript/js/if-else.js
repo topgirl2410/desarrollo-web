6404d1c8c027937b1591e393ae91caa0eb79d957
topgirl2410 <maria.topgirl82@gmail.com>
2022-01-28 13:38:43 +0100

    ejercicios con if else

--- JavaScript/js/if-else.js
+++ JavaScript/js/if-else.js
@@ -2,8 +2,18 @@
 
 const puntuacion = 1002;
 
-if(puntuacion === 1000){
+if(puntuacion == 1000){
     console.log('Si la puntuacion es 1000')
 }else{
     console.log('No, la puntuación no es correcta')
+}
+
+
+const efectivo = 100;
+const carrito = 800;
+
+if (efectivo > carrito) {
+    console.log('El usuario puede pagar');
+} else {
+    console.log('Fondos insuficientes');
 }
