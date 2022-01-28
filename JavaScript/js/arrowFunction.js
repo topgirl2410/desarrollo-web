635027bf410375c153490397b87c85626a510bbd
topgirl2410 <maria.topgirl82@gmail.com>
2022-01-28 13:02:21 +0100

    arrow function

--- JavaScript/js/arrowFunction.js
+++ JavaScript/js/arrowFunction.js
@@ -3,12 +3,64 @@
 // Arrow Functions
 
 const sumar2 = (n1, n2) => console.log(n1 + n2);
-
-
 sumar2(5, 10);
 
-const aprendiendo = (tecnologia) => {
-    console.log(`Aprendiendo ${tecnologia}`)
-}
 
+const aprendiendo = tecnologia => console.log(`Aprendiendo ${tecnologia}`);
 aprendiendo('JavaScript')
+
+
+
+
+const meses = ['Enero', 'febrero', 'marzo', 'abril', 'mayo'];
+
+const carrito = [
+    { nombre: 'Monitor 20 pulgadas', precio: 300 },
+    { nombre: 'Televisión 50 pulgadas', precio: 600 },
+    { nombre: 'Tablet', precio: 300 },
+    { nombre: 'Audifonos', precio: 200 },
+    { nombre: 'Teclado', precio: 50 },
+    { nombre: 'Telefono movil', precio: 500 },
+    { nombre: 'Laptop', precio: 800 },
+    { nombre: 'Altavoces', precio: 300 },
+]
+
+// forEach 
+
+meses.forEach(mes => {
+    if (mes == 'marzo') {
+        console.log('marzo si existe');
+    }
+});
+
+
+resultado = carrito.some(producto => {
+    return producto.nombre === 'Telefono movil'
+});
+
+
+/**resultado = carrito.reduce(function(total, producto){
+    return total + producto.precio
+}, 0);*/
+
+resultado = carrito.reduce((total, producto) => total + producto.precio, 0);
+
+
+// Filter 
+
+resultado = carrito.filter(producto => {
+    return producto.precio > 400
+});
+
+
+resultado = carrito.filter(producto => {
+    return producto.nombre === 'Telefono movil'
+});
+
+resultado = carrito.filter(producto => {
+    return producto.nombre !== 'Telefono movil'
+});
+
+
+
+console.log(resultado);
